753665702a9ba62ccd99f396655bf355c996b4fb
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-02-23 19:35:48 -0300

    revereter commit anterior

--- src/main/java/com/example/todoapi/infra/security/SecurityFilter.java
+++ src/main/java/com/example/todoapi/infra/security/SecurityFilter.java
@@ -26,21 +26,21 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         var token = this.recoverToken(request);
+
         if (token != null) {
             var login = tokenService.validateToken(token);
             UserDetails user = userRepository.findByEmail(login);
 
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
             SecurityContextHolder.getContext().setAuthentication(authentication);
-        } else {
-            throw new RuntimeException("Token is empty.");
         }
+
         filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
-        
         if (authHeader == null)
             return null;
         return authHeader.replace("Bearer ", "");
